c4f9708f2433ed03a618e2d2e00aec214ed9ca97
beambeambeam <supawitmarayat@gmail.com>
2025-04-21 21:54:31 +0700

    chore: move queue to sub folders

--- app/src/test/java/javaroke/queue/QueueLyricTest.java
+++ app/src/test/java/javaroke/queue/QueueLyricTest.java
@@ -1,9 +1,7 @@
-package javaroke;
+package javaroke.queue;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-import javaroke.queue.QueueLyric;
 
 public class QueueLyricTest {
   @Test
