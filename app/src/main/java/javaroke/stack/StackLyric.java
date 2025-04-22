6b9a37cca954c270b8d5ccb4b14bf06e7a44820b
Night <jakkrinmes@gmail.com>
2025-04-22 21:23:18 +0700

    docs: add StackSong and StackLyric comment

--- app/src/main/java/javaroke/stack/StackLyric.java
+++ app/src/main/java/javaroke/stack/StackLyric.java
@@ -3,10 +3,12 @@ package javaroke.stack;
 import javaroke.models.NodeLyric;
 
 public class StackLyric extends StackAbstract<NodeLyric> {
+    // Set up Stact Lyric
     public StackLyric() {
-        super();
+        super(); // like use StackAbstract() to set up StackAbstract<NodeSong> that we are extended
     }
 
+    // Other push function that input as string
     public void push(int time, String line) {
         push(new NodeLyric(time, line));
     }
