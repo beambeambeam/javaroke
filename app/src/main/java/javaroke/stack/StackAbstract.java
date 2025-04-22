defd16dd4a9653b6f6629e9bf3abab14b05e0ba8
Night <jakkrinmes@gmail.com>
2025-04-22 21:17:33 +0700

    docs: add StackAbstract comment

--- app/src/main/java/javaroke/stack/StackAbstract.java
+++ app/src/main/java/javaroke/stack/StackAbstract.java
@@ -20,32 +20,50 @@ public abstract class StackAbstract<T> {
     }
 
     public void push(T data) {
+        // If input data is null, exist this push process
         if (data == null) {
             return;
         }
 
+        // Create newNode as class T,and call Node() self setup function.
         Node<T> newNode = new Node<T>(data);
+
+        // Connect newNode to top, making newNode be lastest top
         newNode.setNext(top);
+
+        // Move top to newNode that live in the last top of this liked list
         top = newNode;
+
+        // If push function work correctly, plus size of queue by 1
         size++;
     }
 
     public T pop() {
+        // If this stact empty, end this pop operation and return as a null value
         if (isEmpty()) {
             return null;
         }
 
+        // Recieve Top data before move node pointer
         T data = top.getData();
+
+        // Move front pointer to top.next
         top = top.getNext();
+
+        // If pop function work correctly, reduce size of queue by 1
         size--;
+
+        // Return Top data that keep before move pointer forward
         return data;
     }
 
     public T peek() {
+        // If this stack empty, end this peek operation and return as a null value
         if (isEmpty()) {
             return null;
         }
 
+        // Return data in Top node
         return top.getData();
     }
 }
