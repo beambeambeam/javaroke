798a68e62875bfc4741c99d3a2d05c2790a9debb
beambeambeam <supawitmarayat@gmail.com>
2025-05-03 17:56:13 +0700

    feat: implement PlayController and update play.fxml for improved UI layout and functionality

--- app/src/main/java/javaroke/gui/play/Controller.java
+++ app/src/main/java/javaroke/gui/play/Controller.java
@@ -1,11 +1,13 @@
-package javaroke.gui;
+package javaroke.gui.play;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javaroke.gui.DataSingleton;
+import javaroke.gui.SceneController;
 import javaroke.songq.SongQueue;
 
-public class PlayController extends SceneController implements Initializable {
+public class Controller extends SceneController implements Initializable {
   DataSingleton data = DataSingleton.getInstance();
   SongQueue externalQueueList = data.getSongQueue();
 
